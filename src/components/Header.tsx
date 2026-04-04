927f9e8a2f28671828d9885f1f7e24628aacab1b
dangero1990 <aeubanksguitar@gmail.com>
2026-04-04 12:33:22 -0400

    test commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,13 @@ import logo from '../assets/logo.svg'
 
 export default function Header () {
     return (
-        <>
+        <header className='flex'>
             <img src={logo} alt="" />
-        </>
+            <select>
+                <option>select unit</option>
+                <option value="">Farienheit</option>
+                <option value="">Celcius</option>
+            </select>
+        </header>
     )
 }
